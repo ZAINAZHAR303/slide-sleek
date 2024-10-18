78888831a3f36b1b9248d4292f7327d5e2a7dd4e
zain <zainazhar303@gmail.com>
2024-10-18 20:51:39 +0500

    theme changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <section className="relative overflow-hidden pt-[150px] pl-4 md:ml-[300px]">
-      <span className="absolute -z-10 rounded-full bg-[#EAEBFD] w-[35rem] h-[35rem] -top-60 -left-60 "></span>
+    <section className="relative -z-20 overflow-hidden bg-white pt-[150px] pl-4 md:ml-[300px]">
+      <span className="absolute -z-10 bg-[#EAEBFD] rounded-full w-[35rem] h-[35rem] -top-60 -left-60 "></span>
       <span className="text-custom-blue text-[18px] font-semibold">
         Hello! I am Zubair Azhar
       </span>
@@ -17,7 +17,7 @@ const Intro = () => {
         will assist you in achieving your desired goals. Please do not hesitate
         to contact us.
       </p>
-      <button className="bg-custom-blue rounded-lg px-8 py-4 text-[14px] font-semibold text-white cursor-pointer transition-all duration-300 hover:bg-custom-gray">
+      <button className="bg-custom-blue  rounded-lg px-8 py-4 text-[14px] font-semibold text-white cursor-pointer transition-all duration-300 hover:bg-custom-gray">
         DOWNLOAD CV
       </button>
     </section>
